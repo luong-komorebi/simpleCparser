81f4cfa87effe52295c5bd6293cbad7c95b2e101
Luong Vo <vo.tran.thanh.luong@gmail.com>
2017-12-28 21:02:15 +0700

    Delete unused files

--- run.bat
+++
@@ -1,4 +0,0 @@
-java -cp JFlex.jar JFlex.Main SimpleC.lex
-java -cp java-cup-11a.jar java_cup.Main -interface < SimpleC.cup
-javac -cp ;jflex.jar;java-cup-11a.jar *.java
-java -cp ;jflex.jar;java-cup-11a.jar MyParser input.txt output.txt
